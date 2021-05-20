3e6e0f4399023ec7c97cb50940b82c32ab5d8d6c
Mikhail <bronnikovmisxha@mail.ru>
2021-05-20 21:08:44 +0300

    f

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import SignIn from './Pages/SignIn';
+function App() {
+  return (
+    <div className="App">
+      <Route path='/signIn' component={SignIn}/>
+    </div>
+  );
+}
+
+export default App;
